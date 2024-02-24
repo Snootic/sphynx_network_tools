cbc84baf790b4186faf8c493f54118ac55906a47
Snootic <snooticonoob@hotmail.com>
2024-02-24 00:06:44 -0300

    fix(deviceFinder): syntax correction and semantics

--- deviceFinder.js
+++ deviceFinder.js
@@ -1,27 +1,28 @@
 const find = require('local-devices');
 
-const macPrefix = '08:AA:3E:8B:7C' // Sphynx default MAC Address Prefix
+const macPrefix = '08:AA:3E:8B:7C'; // Sphynx default MAC Address Prefix
 
 const findSphynx = (devices, macPrefix) => {
   return devices.filter(device => {
-    return device.mac.startsWith(macPrefix);
+    return device.mac === macPrefix;
   });
 };
 
-
 const scanNetwork = async () => {
   try {
+    console.log("Starting scan of local devices...")
+
     const startTime = Date.now(); // Start time of the scanning
 
     const devices = await find();
 
-    // Find ESP32 devices by MAC address prefix
+    // Find Sphynx devices by MAC address
     const sphynxDevices = findSphynx(devices, macPrefix);
 
     const endTime = Date.now(); // End time of the scanning
     const scanTime = endTime - startTime;
 
-    console.log("Find " + sphynxDevices.length + " Sphynx devices in your network")
+    console.log("Find " + sphynxDevices.length + " Sphynx devices in your network");
     console.log(sphynxDevices);
     console.log("Scan completed in " + scanTime + " milliseconds");
   } catch (error) {
