b030654bc26395a911ca56c2934d36678d8bcf87
Snootic <snooticonoob@hotmail.com>
2024-03-06 18:12:22 -0300

    feat[wifi scan]: Code optimization and pt-br location

--- wifi-scan/scan/scan.ino
+++ wifi-scan/scan/scan.ino
@@ -7,152 +7,134 @@
 // This code is meant to be implemented into main.ino at https://github.com/PedroVidalDev/esp32-sphynx
 // this should only be used alone for testing
 
-const char* ssid = "Sphynx-WIFI";
-const char* password = "12345678";
+const char* ssidAP = "Sphynx-WIFI";
+const char* senhaAP = "12345678";
+
 const char* html = R"HTML(
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Sphynx - Connection </title>
-    <script src="https://kit.fontawesome.com/2e476e3619.js" crossorigin="anonymous"></script>
+  <!DOCTYPE html>
+  <html lang="pt-br">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title> Sphynx - Conexão Wifi </title>
 
-</head>
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&family=Roboto:wght@100&display=swap');
+  </head>
+  <style>
 
-    *{
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-        text-decoration: none;
-        list-style: none;
-    }
+      *{
+          padding: 0;
+          margin: 0;
+          box-sizing: border-box;
+          text-decoration: none;
+          list-style: none;
+      }
 
-    html{
-        font-size: 16px;
-        font-family: 'Roboto';
-    }
+      html{
+          font-size: 16px;
+          font-family: 'Roboto';
+      }
 
-    :root{
-        --color-background: #024959;
-        --color-background-bright: #026773;
-        --color-gray: #D9D9D9;
-        --color-submenu: #022534;
-    }
+      :root{
+          --color-background: #024959;
+          --color-background-bright: #026773;
+          --color-gray: #D9D9D9;
+          --color-submenu: #022534;
+      }
 
-    main{
-        position: relative;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        padding: 0 30px;
-        width: 100%;
-        height: 100vh;
-        background-color: var(--color-background);
-    }
+      main{
+          position: relative;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          padding: 0 30px;
+          width: 100%;
+          height: 100vh;
+          background-color: var(--color-background);
+      }
 
-    .container{
-        position: relative;
-        display: flex;
-        align-items: flex-start;
-        justify-content: center;
-        width: 90%;
-        height: 90%;
-        background-color: var(--color-background-bright);
-        border-radius: 15px;
-        overflow-y: scroll;
-    }
+      .container{
+          position: relative;
+          display: flex;
+          align-items: flex-start;
+          justify-content: center;
+          width: 90%;
+          height: 90%;
+          background-color: var(--color-background-bright);
+          border-radius: 15px;
+          overflow-y: scroll;
+      }
 
-    table.content-table{
-        border-collapse: collapse;
-        font-size: 2rem;
-        min-width: 90%;
-        margin-top: 20px;
-    }
+      table.content-table{
+          border-collapse: collapse;
+          font-size: 2rem;
+          min-width: 90%;
+          margin-top: 20px;
+      }
 
-    table.content-table thead tr{
-        background-color: var(--color-background);
-        color: white;
-        text-align: center;
-        font-weight: bold;
-    }
+      table.content-table thead tr{
+          background-color: var(--color-background);
+          color: white;
+          text-align: center;
+          font-weight: bold;
+      }
 
-    table.content-table th, table.content-table td {
-        padding: 12px 15px;
-        color: white;
-    }
+      table.content-table th, table.content-table td {
+          padding: 12px 15px;
+          color: white;
+      }
 
-    table.content-table th, table.content-table td a{
-        color: white;
-        transition: .5s;
-    }
+      table.content-table th, table.content-table td a{
+          color: white;
+          transition: .5s;
+      }
 
-    .correct:hover{
-        color: green;
-    }
+      .correct:hover{
+          color: green;
+      }
 
-    .incorrect:hover{
-        color: red;
-    }
+      .incorrect:hover{
+          color: red;
+      }
 
-    table.content-table tbody tr{
-        border-bottom: 1px solid var(--color-background);
-        text-align: center;
-    }
-</style>
-<body>
-    <header></header>
-    <main>
-        <div class="container">
-            <table class="content-table">
-                <thead>
-                    <tr>
-                        <th> Name </th>
-                        <th> Signal </th>
-                        <th> Connect </th>
-                        <th> Disconnect </th>
-                    </tr>
-                </thead>
-                <tbody>)HTML";
+      table.content-table tbody tr{
+          border-bottom: 1px solid var(--color-background);
+          text-align: center;
+      }
+  </style>
+  <body>
+      <header></header>
+      <main>
+          <div class="container">
+              <table class="content-table">
+                  <thead>
+                      <tr>
+                          <th> Nome </th>
+                          <th> Força do Sinal </th>
+                          <th> Conectar </th>
+                          <th> Desconectar </th>
+                      </tr>
+                  </thead>
+                  <tbody>)HTML";
 
 const char* htmlFooter = R"footer(                </tbody>
             </table>
         </div>
     </main>
     <footer></footer>
-</body>
-</html>)footer";
+    </body>
+    </html>)footer";
 
-WiFiServer server(80);
-WiFiClient currentClient;
+WiFiServer serverAP(80);
+WiFiClient clientAtual;
+bool apServerUp = false;
 
-void setup() {
-  Serial.begin(115200);
-
-  Serial.print("Setting AP (Access Point)…");
-  WiFi.softAP(ssid, password);
-
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
-
-  server.begin();
-
-  if (!MDNS.begin("Sphynx-dev")) {
-    Serial.println("Error starting mDNS");
-    return;
-  }
-}
-
-void handleClientRequest(WiFiClient client) {
+void setupWiFi(WiFiClient client) {
   client.println("HTTP/1.1 200 OK");
   client.println("Content-type:text/html");
   client.println();
   client.println(html);
-  int numNetworks = WiFi.scanNetworks();
-  for (int i = 0; i < numNetworks; ++i) {
+  int numRedes = WiFi.scanNetworks();
+  for (int i = 0; i < numRedes; ++i) {
     client.println("                    <tr>");
     client.print("                        <td>");
     client.print(WiFi.SSID(i));
@@ -171,30 +153,41 @@ void handleClientRequest(WiFiClient client) {
   client.println(htmlFooter);
 }
 
-void loop() {
-  WiFiClient client = server.available();
-  if (client) {
-    Serial.println("New Client.");
-    currentClient = client;
-    String currentLine = "";
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        if (c == '\n') {
-          if (currentLine.length() == 0) {
-            handleClientRequest(currentClient);
-            break;
-          } else {
-            currentLine = "";
-          }
-        } else if (c != '\r') {
-          currentLine += c;
-        }
-      }
-    }
-    client.stop();
-    Serial.println("Client disconnected.");
+void setup() {
+  Serial.begin(115200);
+
+  Serial.print("Configurando Acess Point...");
+  WiFi.softAP(ssidAP, senhaAP);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("Endereco IP do AP: ");
+  Serial.println(IP);
+
+  serverAP.begin();
+  apServerUp = true;
+
+  if (!MDNS.begin("Sphynx-dev")) {
+  Serial.println("Erro ao iniciar mDNS");
+  return;
   }
-  delay(500);
+}
+
+void loop() {
+  if (apServerUp){
+    Serial.println("Server is up!");
+    WiFiClient client = serverAP.available();
+    if (client) {
+      Serial.println("Novo Client.");
+      clientAtual = client;
+      while (client.connected()) {
+        if (client.available()) {
+            setupWiFi(clientAtual);
+            break;
+            }
+      }
+      client.stop();
+      Serial.println("Client Desconectado..");
+    }
+    delay(500);
+  }
 }
